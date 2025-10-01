dd4bf4132a0c2d43fa1b4ef7f72be1741825097b
DabAZ <dabaz@dabaz.me>
2025-10-02 04:00:03 +0800

    Add all components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+import SudokuGrid from "@/components/sudoku/SudokuGrid";
+import MistakeCounter from "@/components/sudoku/MistakeCounter";
+import Timer from "@/components/sudoku/Timer";
+import Pause from "@/components/sudoku/Pause";
+import NumericButtons from "@/components/sudoku/NumericButtons";
+
 export default function Page() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold font-display">Play Free Sudoku online - solve web sudoku puzzles</h1>
+    <div className="space-y-4 lg:space-y-0 lg:flex lg:flex-row-reverse lg:items-start lg:gap-6">
+      <div className="flex items-center gap-4 justify-between">
+        <Timer />
+        <div className="flex items-center gap-x-2">
+          <MistakeCounter />
+          <Pause />
+        </div>
+      </div>
+      <div className="space-y-3">
+        <SudokuGrid />
+        <NumericButtons />
+      </div>
     </div>
   )
 }
